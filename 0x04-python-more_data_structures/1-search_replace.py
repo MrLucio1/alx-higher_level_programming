21f857decaeff2b6f8fadacc798fb1c8988ff090
MrLucio1 <azakari180@gamil.com>
2022-12-11 11:48:52 -0800

    correction

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-	return (list(map(lambda new: new if new != search else replace, my_list)))
+    def find_search(element):
+        return element if element != search else replace
+    return liest(map(find_search, my_list))
